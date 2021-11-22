9491ac70154598b70e9499a2b5b47bfe53a65c20
HonorineF <honorine.f@hotmail.com>
2021-11-22 14:03:40 +0100

    Squashed commit of the following:
    commit 59bcc4199211df9c6eb1310c3d5b0e8e2595614a
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Mon Nov 22 14:00:55 2021 +0100
    
        logs
    
    commit 59e8e4a09a1acec40f72a902e440fc0cbef0f863
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Mon Nov 22 13:47:39 2021 +0100
    
        bl
    
    commit 2da3361cc38414edc4af59052356b2faf85655e7
    Author: Vincent Reynaert <v.vincent59230@laposte.net>
    Date:   Mon Nov 22 11:54:37 2021 +0100
    
        Grab ok
    
    commit 749ef8a748f4ff64ec9341563c3727dc891881d5
    Author: Vincent Reynaert <v.vincent59230@laposte.net>
    Date:   Mon Nov 22 11:31:44 2021 +0100
    
        jyf
    
    commit 3a823d83f94b114654327e9449165978099f3d6b
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Mon Nov 22 11:07:35 2021 +0100
    
        eg
    
    commit 6f46de7117c47b65bc136759710327ec3084b06b
    Merge: cc27de8 4211a93
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Mon Nov 22 10:57:55 2021 +0100
    
        Merge branch 'HonorineTest' of https://github.com/BaptisteHollebecq/TP_VR into HonorineTest
    
    commit cc27de8d18a422d77f227168011cd878c3c7e660
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Mon Nov 22 10:55:04 2021 +0100
    
        th
    
    commit 4211a933af5f2cd5b898cf3a7f567074ba40134b
    Author: Vincent Reynaert <v.vincent59230@laposte.net>
    Date:   Mon Nov 22 10:31:10 2021 +0100
    
        rtfg
    
    commit a8ac133a05048af72d8d808466a934030d905a3b
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Mon Nov 22 10:16:45 2021 +0100
    
        tyc
    
    commit f5322844c840faa41800be431d5e14aa547a26fa
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Mon Nov 22 10:02:17 2021 +0100
    
        ghfc
    
    commit 3a799dea9ee6d2b4386f2cc268c59dd341c38585
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Mon Nov 22 09:53:29 2021 +0100
    
        test
    
    commit a68dab2d7b66c7e450d76654ade76971996a8570
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Thu Nov 18 15:56:11 2021 +0100
    
        clear
    
    commit e51aaac6028dfe5e17c374f3b49c32311ac2d7d7
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Wed Nov 17 11:45:23 2021 +0100
    
        fix
    
    commit b78303efcfd081d621cb0aa1e6b36d01a89ecd20
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Wed Nov 17 11:33:04 2021 +0100
    
        Scene
    
    commit 9025df9988470d6f185bfd90b391bf88185e4d24
    Author: HonorineF <honorine.f@hotmail.com>
    Date:   Wed Nov 17 11:27:59 2021 +0100
    
        Grab and drop object

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    [SerializeField]
+    public bool Holding = false;
+
+    private GameObject _object = null;
+    private Rigidbody _rb;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _object = other.gameObject;
+        Debug.Log("TRIGGER OBJECT");
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        _object = null;
+        Debug.Log("TRIGGER OFF OBJECT");
+    }
+
+    public void GrabObject()
+    {
+        if (_object != null)
+        {
+            _rb = _object.GetComponent<Rigidbody>();
+            _rb.isKinematic = true;
+            _object.transform.parent = this.transform;
+            Holding = true;
+            Debug.Log("GRAB OBJECT");
+        }
+    }
+
+    public void DropObject()
+    {
+        _object.transform.parent = null;
+        _rb.isKinematic = false;
+        Holding = false;
+        Debug.Log("DROP OBJECT");
+    }
+}
