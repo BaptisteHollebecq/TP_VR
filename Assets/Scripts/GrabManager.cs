f0bd5bbffa3bc776594d7c8dff3ab6e4ba5eb0ee
BaptisteHollebecq <55829266+BaptisteHollebecq@users.noreply.github.com>
2021-12-01 11:13:07 +0100

    f

--- Assets/Scripts/GrabManager.cs
+++ Assets/Scripts/GrabManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GrabManager : MonoBehaviour
 {
     public Grab leftHand;
     public Grab rightHand;
+
+
+    public void Death()
+    {
+        // WHALLA FAIRE LES MENU
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
